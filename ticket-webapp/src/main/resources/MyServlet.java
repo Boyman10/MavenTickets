ac4cd76b111880a51686a6a77d2c7e5ba3f8977e
Boyman10 <contact@yasi.fr>
2018-02-18 17:29:53 +0100

    Working on Maven with plugins and lifecycle

---
+++ ticket-webapp/src/main/resources/MyServlet.java
@@ -0,0 +1,41 @@
+//https://maven.apache.org/ref/3.5.0/maven-core/lifecycles.html
+//https://maven.apache.org/ref/3.5.0/maven-core/default-bindings.html
+//https://maven.apache.org/plugins/index.html
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		@Deprecated
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
